d78198a027ce149a3e2284effce8b731f683b89f
sh6986 <okok960411@gmail.com>
2022-01-13 23:53:19 +0900

    시퀄라이즈 설정

---
+++ myapp/models/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+db.sequelize = sequelize;
+
+module.exports = db;
